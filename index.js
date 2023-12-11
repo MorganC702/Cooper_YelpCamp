f7d8d6870ea4724f935b882f3316243b3fe9d6d6
GOD <morgan.roycooper@gmail.com>
2023-12-10 17:44:40 -0800

    added reviewing capabilities

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const methodOverride = require('method-override');
 const catchAsync = require('./helpers/catchAsync');
 const ExpressError = require('./helpers/ExpressErrors');
 const Joi = require('joi');
-const { campgroundSchema } = require('./schemas')
+const { campgroundSchema, reviewSchema } = require('./schemas')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -37,7 +38,16 @@ const validateCampground = (req, res, next) => {
         next()
     }
 }
-
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body)
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    else {
+        next()
+    }
+}
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -63,7 +73,7 @@ app.post('/campgrounds', validateCampground, catchAsync (async (req, res, next) 
 
 
 app.get('/campgrounds/:id', catchAsync (async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews')
     res.render('campgrounds/show', { campground })
 }))
 
@@ -88,17 +98,35 @@ app.delete('/campgrounds/:id', catchAsync (async (req, res) => {
 }))
 
 
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review)
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
+ 
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/campgrounds/${id}`)
+}))
+
+
 app.all('*', (req, res, next) => {
     next( new ExpressError('Page Not Found', 404))
 })
-
+ 
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err
     if(!err.message) err.message = "No message is available, but error did occur"
     res.status(statusCode).render('error', { err })
 })
-
+ 
 
 app.listen(3000, () => {
     console.log("Serving on port 3000");
